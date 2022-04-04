f366d9edb8c9aa3ebe53a37082a3d9dc25cc54ad
Sahri-YahyaAbdelkadouss <yahyasahrii2@gmail.com>
2022-04-04 18:29:04 +0000

    initial commit before switching to Dev branch

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./requests";
+
+function App() {
+  return (
+    <div className="App">
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix-clone/src/components/Row.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, isLargeRow } from "react";
+import axios from "../axios";
+import "./Row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  //   this code runs when a spesific condition is met
+  useEffect(() => {
+    // any outside variable is a depandancy must be added in []
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      {/* /* title */}
+      <h2> {title} </h2>
+      {console.log(movies)}
+      <div className="row__posters">
+        {/*several row_posters */}
+        {movies.map((movie) => (
+          // why it's not working without () ==> change it to {} is not feasible
+          //   console.log(movie);
+          //   console.log(isLargeRow, "isLargeRow");
+          //   const poster_path = "poster_path";
+          //   const backdrop_path = "backdrop_path";
+          //   console.log(`${isLargeRow ? poster_path : backdrop_path}`);
+          //   console.log("url :" , `${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`);
+          //console.log(`${base_url}${movie.poster_path}`);
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            //src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* /* container  ->  posters */}
+    </div>
+  );
+}
+
+export default Row;
