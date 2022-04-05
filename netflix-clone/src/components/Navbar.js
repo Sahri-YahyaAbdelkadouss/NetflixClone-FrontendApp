91a98ff7c8c4c8543b766be8e28a255361100172
Sahri-YahyaAbdelkadouss <yahyasahrii2@gmail.com>
2022-04-05 01:55:02 +0000

    Navbar finished

--- netflix-clone/src/components/Navbar.js
+++ netflix-clone/src/components/Navbar.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 function Navbar() {
+  const [show, handleShow] = useState(false);
+  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
 
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
 
-    
   return (
-    <div className="nav">
+    <div className = {`nav ${show && "nav_black"}`}> 
       <img
         className="nav_logo"
-        src="https://imgix.bustle.com/uploads/image/2017/8/29/c8c8077a-10fc-44d5-93f0-da4e592a299e-netflix-logo-print_pms.jpg?w=1200&h=630&fit=crop&crop=faces&fm=jpg"
+        // src="https://imgix.bustle.com/uploads/image/2017/8/29/c8c8077a-10fc-44d5-93f0-da4e592a299e-netflix-logo-print_pms.jpg?w=1200&h=630&fit=crop&crop=faces&fm=jpg"
+        src="https://th.bing.com/th/id/R.7019a854d877f915be14071b6a2ed660?rik=QnOuOoW74ADZ8Q&riu=http%3a%2f%2fwww.zupimages.net%2fup%2f16%2f31%2fm3sd.png&ehk=KfnKfJYa7lYGKrytV9RsfKf7%2buM3qsH%2fPlrcejgwKsE%3d&risl=&pid=ImgRaw&r=0"
         alt=""
       />
 
